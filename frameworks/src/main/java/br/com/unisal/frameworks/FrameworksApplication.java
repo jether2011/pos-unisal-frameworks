b104f7978067353787e1db3ff35ace9c2976db25
pabreu1 <paulo.abreu@gingaone.com>
2017-03-17 23:54:51 -0300

    first commit

---
+++ frameworks/src/main/java/br/com/unisal/frameworks/FrameworksApplication.java
@@ -0,0 +1,12 @@
+package br.com.unisal.frameworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworksApplication.class, args);
+	}
+}
